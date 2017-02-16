7b7d601df8aef8b6e65d2fa567584afadef6076f
Peter Grund <peter.grund@bisnode.de>
2017-02-16 08:19:43 +0100

    introduced born, dead, ... states

--- gameoflife/js/app.js
+++ gameoflife/js/app.js
@@ -3,9 +3,9 @@ class Cell extends React.Component {
     super(props);
   }
   render() {
-    return (
-      <div className="cell">
-        <span className={"glyphicon" + (this.props.alive ? " glyphicon-user" : "")}
+      return (
+      <div className={"cell " + this.props.changed}>
+        <span className={"glyphicon" + (this.props.alive ? ' glyphicon-user': '')}
         title={this.props.alive}></span>
       </div>
     );
@@ -21,7 +21,8 @@ class Game extends React.Component {
     this.state = {
       cells : this.generateCells(this.props.dimension),
       generation: 0,
-      timer: false
+      timer: false,
+      message: ''
     }
 
     this.generateCells = this.generateCells.bind(this);
@@ -34,7 +35,7 @@ class Game extends React.Component {
   generateCells(dim) {
     return [...new Array(dim).keys()].map(row =>
       [...new Array(dim).keys()].map( ele =>
-        Math.random() < .5
+        (Math.random() < .5 ? 0 : 1)
       ));
   }
 
@@ -45,30 +46,42 @@ class Game extends React.Component {
       for(let i1=i-1; i1 <= i+1; i1++) {
         for(let j1=j-1; j1 <= j+1; j1++) {
           if(i1 < 0 || j1 < 0 || i1 >= _this.props.dimension || j1 >= _this.props.dimension || (i==i1 && j==j1)) continue;
-          if(_this.state.cells[i1][j1])amount ++
+          if(_this.state.cells[i1][j1] > 0 )amount ++
         }
       }
       return amount;
     }
-    function cellInNextIteration(neighbors, boolValue) {
+    function cellInNextIteration(neighbors, value) {
       switch (neighbors) {
         case 0: case 1:
             //Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
-            return false;
+            if(value > 0) return -1;
         case 2:
             //Any live cell with two or three live neighbours lives on to the next generation.
-            return boolValue;
+            if(value > 0) {
+              return 1;
+            } else {
+              return 0;
+            }
         case 3:
             //Any live cell with two or three live neighbours lives on to the next generation.
             //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-            return true;
+            if(value > 0) {
+              return 1;
+            } else {
+              return 2;
+            }
         default:
             //Any live cell with more than three live neighbours dies, as if by overpopulation.
-            return false;
+            if(value > 0) {
+              return -1;
+            } else {
+              return 0;
+            }
       }
     }
-    function anyCellsLeft(arr) {
-      return arr.map(row => row.some(val => val)).some(val => val);
+    function noCellsLeft(arr) {
+      return !arr.map(row => row.some(val => val)).some(val => val>0);
     }
 
     let next = [];
@@ -80,19 +93,29 @@ class Game extends React.Component {
       next.push(row);
     };
 
+    if(this.state.cells.every((row, i) => row.every((cell, j) => next[i][j] == cell)) &&
+      next.every((row, i) => row.every((cell, j) => this.state.cells[i][j] == cell))) {
+        this.setState({
+          message: 'Staled state, no more changes in game ...'
+        });
+        this.handlePause();
+    }
     this.setState({
       generation: (this.state.generation+1),
       cells: next
     });
-    if(!anyCellsLeft(next)) {
-      console.log("no cells left");
+    if(noCellsLeft(next)) {
+      this.setState({
+        message: 'No cells left in game ...'
+      });
       this.handlePause();
     }
   }
 
   handleRun() {
     this.setState({
-      timer: setInterval(this.nextIteration, this.props.interval * 1000)
+      timer: setInterval(this.nextIteration, this.props.interval * 1000),
+      message: ''
     })
   }
   handlePause() {
@@ -104,16 +127,32 @@ class Game extends React.Component {
   handleClear(){
     this.setState({
       cells : this.generateCells(this.props.dimension),
-      generation: 0
+      generation: 0,
+      message: ''
     });
   }
 
   render() {
+    console.log(this.state.cells[0]);
     function renderRow(row) {
+      function changed(val) {
+        switch (val) {
+          case 0:
+            return 'dead';
+          case 1:
+            return 'alive';
+          case -1:
+            return 'died';
+          case 2:
+            return 'born';
+          default:
+            return 'unknown';
+        }
+      }
       return (
         <div className="cellRow">
-          { row.map( boolValue =>
-            <Cell alive={boolValue} />
+          { row.map( cell =>
+            <Cell alive={cell > 0} changed={changed(cell)}/>
           )}
         </div>
       );
@@ -122,7 +161,8 @@ class Game extends React.Component {
       <div className="game text-center">
        {this.state.cells.map (row => renderRow(row))}
        <div>
-         <span>Generation: {this.state.generation}</span>
+         <p>Generation: {this.state.generation}</p>
+         <p className="message">{this.state.message}</p>
        </div>
        <div className="buttons">
          <button className="btn btn-success" onClick={this.handleRun} title="play">
